1fda383ea26d82cfb53b18c9c77941efbe0fa74a
chris huson and julia sheremet <chris@>
2017-11-29 10:44:48 -0800

    add notes to understand js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-$(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
+$(document).ready(function() { // when the document is loaded
+  $("#formOne").submit(function(event) { // when the form with the id formOne is submitted
 
-    $(".person1").text(person1Input.toUpperCase());
+    var person1Input = $("input#person1").val(); //create a variable called person1Input with the content of what was in the field with the id person1
 
-    $("#story").show();
+    $(".person1").text(person1Input.toUpperCase()); //give the html tag that carries the class .person1 the text of the variable person1Input but uppercase that word first.
 
-    event.preventDefault();
+    $("#story").show(); //then, show the html element that carries the id "story"
+
+    event.preventDefault(); //do not refresh page
+
   });
 });
